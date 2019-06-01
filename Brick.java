5590d3f44a8479f06c5b867182d89632e813da11
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-31 20:38:27 -0400

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -1,1 +1,42 @@
+package BreakoutGame
 
+import javax.swing.ImageIcon;
+
+public class Brick extends Sprite {
+	
+	private boolean destroyed;
+	
+	//This is the Brick class.//
+	//In the destroyed variable we keep the state of a brick.//
+	
+	public Brick(int x, int y) {
+		
+		initBrick(x, y);
+	}
+	
+	private void initBrick(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+			
+		destroyed = false;	
+		
+		loadImage();
+		getImageDimensions();
+	}
+	
+	private void loadImage() {
+		
+		ImageIcon ii = new ImageIcon("src/resources/brick.png");
+		image = ii.getImage();
+	}
+	
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+	
+	public void setDestroyed(boolean val) {
+		
+		destroyed = val;
+	}
+}

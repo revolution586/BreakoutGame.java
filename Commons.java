c26a6af75ba67e748f34ab979688658893cde1c6
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-31 20:26:54 -0400

    Update Commons.java

--- Commons.java
+++ Commons.java
@@ -7,7 +7,7 @@ public interface Commons {
   public static final int BOTTOM_EDGE = 390;
   public static final int N_OF_BRICKS = 30;
   public static final int INIT_PADDLE_X = 200;
-	public static final int INIT_PADDLE_Y = 360;
+  public static final int INIT_PADDLE_Y = 360;
 	public static final int INIT_BALL_X = 230;
 	public static final int INIT_BALLUY = 355;
 	public static final int DELAY = 1000;
@@ -19,4 +19,5 @@ public interface Commons {
 //When the ball passes the BOTTOM_EDGE, the game is over.//
 //The N_OF_BRICKS is the number of bricks in the game//
 //The INIT_PADDLE_X and INIT_PADDLE_Y are initial coordinates of the paddle object//
-//The INIT_BALL_X 
+//The INIT_BALL_X and INIT_BALL_Y are initial coordinates of the ball object//
+//The DELAY is the initial delay in milliseconds before task is to be exectures and the PERIOD is the time in milliseconds between successive task executions that form game cycles//

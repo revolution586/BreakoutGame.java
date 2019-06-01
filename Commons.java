9a1b2795958738a6552ae7817ab4fb68164c31ae
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-31 20:24:48 -0400

    Update Commons.java

--- Commons.java
+++ Commons.java
@@ -1,1 +1,22 @@
+package BreakoutGame;
 
+public interface Commons {
+  
+  public static final int WIDTH = 300;
+  public static final int HEIGHT = 400;
+  public static final int BOTTOM_EDGE = 390;
+  public static final int N_OF_BRICKS = 30;
+  public static final int INIT_PADDLE_X = 200;
+	public static final int INIT_PADDLE_Y = 360;
+	public static final int INIT_BALL_X = 230;
+	public static final int INIT_BALLUY = 355;
+	public static final int DELAY = 1000;
+	public static final int PERIOD = 10;
+}
+
+//The Commons.java file has some common constants//
+//The WIDTH and HEIGHT constants store the dimensions of the board//
+//When the ball passes the BOTTOM_EDGE, the game is over.//
+//The N_OF_BRICKS is the number of bricks in the game//
+//The INIT_PADDLE_X and INIT_PADDLE_Y are initial coordinates of the paddle object//
+//The INIT_BALL_X 

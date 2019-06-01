7aa8e0920443a7f44032d3f4ec9798c40ea0c59b
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-31 21:38:12 -0400

    Update Breakout.java

--- Breakout.java
+++ Breakout.java
@@ -1,1 +1,33 @@
+package BreakoutGame;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Breakout extends JFrame {
+	
+	public Breakout() {
+		
+		initUI();
+	}
+	
+	private void initUI() {
+		
+		add(new Board());
+		setTitle("Breakout");
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(Commons.WIDTH, Commons.HEIGHT);
+		setLocationRelativeTo(null);
+		setRezisable(false);
+	}
+	
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(() -> {
+			Breakout game = new Breakout();
+			game.setVisible(true);
+		});
+	}
+}
+
+//And finally, this is the Breakout class which has the main entry method where window for where the game is taken place will show.//

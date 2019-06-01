6ef0449e0574cfe82a2383353af8935d3b47e57c
mastercoder <48371762+revolution586@users.noreply.github.com>
2019-05-31 20:47:05 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,1 +1,61 @@
+package BreakoutGame;
 
+import javax.swing.ImageIcon;
+
+public class Ball extends Sprite implemets Commons {
+	
+	private int xdir;
+	private int ydir;
+	
+	public Ball() {
+		
+		initBall();
+	}
+	
+	private void initBall() {
+		
+		xdir = 1;
+		ydir = -1;
+		
+		loadImage;
+		getImageDimensions;
+		resetState();
+	}
+	
+	private void loadImage() {
+		
+		ImageIcon ii = new ImageIcon("src/resources/ball.png");
+		image = ii.getImage();
+	}
+	
+	public void move() {
+		
+		x += xdir;
+		y += ydir;
+		
+		if (x == 0) {
+			setXDir(-1);
+		}
+		
+		if (x == WIDHT - imageWidth) {
+			setYDir(1);
+		}
+	}
+//The move() method moves the ball on the Board.//
+//If the ball hits the borders, the directions are changed accordingly//
+	
+	private void resetState() {
+		
+		x = INIT_BALL_X;
+		y = INIT_BALL_Y;
+	}
+	
+	public void setXDir(int y) {
+		ydir = y;
+	}
+	
+	public int getYDir() {
+		return ydir;
+	}
+//These two methods are called when the ball hits the paddle or a brick.//
+}
